6bef4ac11e7d6bf5007e04b91fc39b6ade83b97b
Mahesh Chandra <118873892+ChandraMahesh24@users.noreply.github.com>
2024-07-13 12:29:43 +0530

    Create ImageSliderAdapter.java

---
+++ ImageSliderAdapter.java
@@ -0,0 +1,51 @@
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.healthcare.R;
+
+import java.util.List;
+
+public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.SliderViewHolder> {
+
+
+    private List<Integer> images;
+    private Context context;
+
+    public ImageSliderAdapter(List<Integer> images, Context context) {
+        this.images = images;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.slide_item, parent, false);
+        return new SliderViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
+        holder.imageView.setImageResource(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    class SliderViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageViewSlider);
+        }
+    }
+}
